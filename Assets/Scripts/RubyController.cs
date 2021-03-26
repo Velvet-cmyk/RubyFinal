6c1003562b86a3c9349d66b8d682c4da24fb89ee
Velvet-cmyk <you@example.com>
2021-03-25 20:25:16 -0400

    March 25, 2021 8:25 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    float horizontal; 
+    
     // Start is called before the first frame update
     void Start()
     {
