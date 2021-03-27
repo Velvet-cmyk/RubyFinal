a99c265cbe57fa27ed4d0a8c66a4a76331c8cd10
Velvet-cmyk <you@example.com>
2021-03-27 00:58:07 -0400

    March 27, 2021 12:58 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -6,9 +6,13 @@ public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        RubyControlller controller = other.GetComponent<RubyController>();
+        RubyController controller = other.GetComponent<RubyController>();
 
-        
+        if (controller !=null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject); 
+        }
 
     }
 
