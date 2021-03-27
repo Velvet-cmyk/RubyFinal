faa05511a0031d1c1be6487c346dd1681e418ed6
Velvet-cmyk <you@example.com>
2021-03-27 00:57:04 -0400

    March 27, 2021 12:57 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -6,7 +6,9 @@ public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object that entered the trigger :" + other);
+        RubyControlller controller = other.GetComponent<RubyController>();
+
+        
 
     }
 
