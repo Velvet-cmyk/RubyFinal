636d80c686a934618fcfe7149784a39579827820
Velvet-cmyk <you@example.com>
2021-03-27 08:59:39 -0400

    March 27, 2021 8:59 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -38,7 +38,11 @@ public class RubyController : MonoBehaviour
             lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
-                Debug.Log("Raycast has hit the object" + hit.collider.gameObject);
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
             }
         }
         horizontal = Input.GetAxis("Horizontal");
