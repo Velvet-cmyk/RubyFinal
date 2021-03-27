e84dc1696626fb38ddbec1b0aa435649d11b0b05
Velvet-cmyk <you@example.com>
2021-03-27 04:28:26 -0400

    March 27, 2021 4:28 AM

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rb; 
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent
+        rb = GetComponent<Rigidbody2D>();
     }
 
+    public void Launch(Vector2 direction, float force)
+    {
+        rb.AddForce(direction * force);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Projectile Collision with" + other.gameObject);
+        Destroy(gameObject);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
