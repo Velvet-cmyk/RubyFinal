7d3a1c24d5b148ad494c91ce66be6c0e3616ab0a
Velvet-cmyk <you@example.com>
2021-03-27 07:29:17 -0400

    March 27, 2021 7:29 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
+
 {
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
