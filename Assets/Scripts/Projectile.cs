f13cfa0255fe198ae36227dc348b9228f185be5d
Velvet-cmyk <you@example.com>
2021-03-27 04:57:53 -0400

    March 27, 2021 4:57 AM

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -18,13 +18,13 @@ public class Projectile : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D other)
     {
-        EnemyController e= other.collider.GetComponent<EnemyController>(); 
-        if (e !=null)
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
         {
             e.Fix();
         }
 
-        Debug.Log("Projectile Collision with" + other.gameObject);
+
         Destroy(gameObject);
     }
     // Update is called once per frame
