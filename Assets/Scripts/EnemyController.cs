1d9923075067aa8fd8052e45c02e036c3d8343bd
Velvet-cmyk <you@example.com>
2021-03-27 04:53:52 -0400

    March 27, 2021 4:53 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,65 +4,66 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed; 
+    public float speed;
     public bool vertical;
-    public float changeTime=3.0f; 
-    Rigidbody2D rb; 
-    float timer; 
-    int direction=1; 
+    public float changeTime = 3.0f;
+    Rigidbody2D rb;
+    float timer;
+    int direction = 1;
     Animator anim;
+    bool broken;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        timer=changeTime;
-        anim=GetComponent<Animator>(); 
+        timer = changeTime;
+        anim = GetComponent<Animator>();
     }
     //this function is called once per frame
     void Update()
     {
-        if(!broken)
+        if (!broken)
         {
-            return; 
+            return;
         }
-        
-        timer -=Time.deltaTime;
-        
-        if (timer<0)
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
         {
-            direction=-direction;
-            timer=changeTime;
+            direction = -direction;
+            timer = changeTime;
         }
     }
-    
+
     void FixedUpdate()
     {
-        Vector2 position=rb.position;
-        if(vertical)
+        Vector2 position = rb.position;
+        if (vertical)
         {
-            position.y=position.y+Time.deltaTime*speed*direction;; 
-            anim.SetFloat("Move X",0);
-            anim.SetFloat("Move Y",direction); 
+            position.y = position.y + Time.deltaTime * speed * direction; ;
+            anim.SetFloat("Move X", 0);
+            anim.SetFloat("Move Y", direction);
         }
         else
         {
             {
-                position.x=position.x+Time.deltaTime*speed*direction;; 
+                position.x = position.x + Time.deltaTime * speed * direction; ;
                 anim.SetFloat("Move X", direction);
-                anim.SetFloat("Move Y",0); 
+                anim.SetFloat("Move Y", 0);
             }
         }
-         
-        rb.MovePosition(position); 
+
+        rb.MovePosition(position);
     }
     void OnCollisionEnter2D(Collision2D other)
     {
-        RubyController player=other.gameObject.GetComponent<RubyController>(); 
+        RubyController player = other.gameObject.GetComponent<RubyController>();
 
-        if (player !=null)
+        if (player != null)
         {
-            player.ChangeHealth(-1); 
+            player.ChangeHealth(-1);
         }
     }
 }
