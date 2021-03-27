adbf757b38bc78f3f0e23914a0a6321c7986f982
Velvet-cmyk <you@example.com>
2021-03-27 01:56:39 -0400

    March 27, 2021 1:56 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class enemycontroller : MonoBehaviour
 {
     public float speed; 
-    Rigibody2D rb; 
+    Rigidbody2D rb; 
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class enemycontroller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 position=rigibody2D.position;
+        Vector2 position=rigidbody2D.position;
         position.x=position.x+Time.deltaTime*speed; 
         rigibody2d.MovePosition(position); 
     }
