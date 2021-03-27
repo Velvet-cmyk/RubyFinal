f17fca320f66b7cf770986aa8e9c964688edb30c
Velvet-cmyk <you@example.com>
2021-03-27 00:24:40 -0400

    March 27, 2021 12:24 AM

---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+   void OnTriggerEnter2d(Collider2D other)
+   
+}
