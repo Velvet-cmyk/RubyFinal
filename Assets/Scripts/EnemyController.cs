6b918d0629531a2d7f605bba7a4d87dea61132f3
Velvet-cmyk <you@example.com>
2021-03-27 01:57:02 -0400

    March 27, 2021 1:57 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,7 +16,7 @@ public class enemycontroller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 position=rigidbody2D.position;
+        Vector2 position=GetComponent<Rigidbody2D>().position;
         position.x=position.x+Time.deltaTime*speed; 
         rigibody2d.MovePosition(position); 
     }
