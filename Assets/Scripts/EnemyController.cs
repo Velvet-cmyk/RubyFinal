f8c83cdf31c32ec247479e9bde31ff9dedf51059
Velvet-cmyk <you@example.com>
2021-03-27 02:12:59 -0400

    March 27, 2021 2:12 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -43,4 +43,13 @@ public class EnemyController : MonoBehaviour
          
         rb.MovePosition(position); 
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player=other.gameObject.GetComponent<RubyController>(); 
+
+        if (player !=null)
+        {
+            player.ChangeHealth(-1); 
+        }
+    }
 }
