6465dcd23d444a3716b839cda17a8195d0976daa
Velvet-cmyk <you@example.com>
2021-03-27 02:05:54 -0400

    March 27, 2021 2:05 AM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed; 
-    public bool vertical; 
+    public float speed=3.0f; 
+    public bool vertical;
+    public float changeTime=3.0f 
     Rigidbody2D rb; 
+    float timer; 
+    int direction=1; 
 
     // Start is called before the first frame update
     void Start()
