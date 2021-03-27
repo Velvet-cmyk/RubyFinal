181605af9dbd1ded405573c1746df4e8488a21fa
Velvet-cmyk <you@example.com>
2021-03-27 04:25:36 -0400

    March 27, 2021 4:25 AM

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
