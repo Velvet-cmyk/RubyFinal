5632221b1f2d93954973ce77a4c85ed60e1d82f4
Velvet-cmyk <you@example.com>
2021-03-30 21:19:57 -0400

    March 30, 2021 9:19 PM

--- Assets/Scripts/EnemyControllerVert.cs
+++ Assets/Scripts/EnemyControllerVert.cs
@@ -2,17 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyControllerVert : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    Rigidbody2D rb;
+    float timer;
+    int direction = 1;
+    Animator anim;
+    bool broken = true;
+    public ParticleSystem smokeEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
+    //this function is called once per frame
     void Update()
     {
-        
+
+
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+        if (!broken)
+        {
+            return;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        Vector2 position = rb.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction; ;
+            anim.SetFloat("Move X", 0);
+            anim.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            {
+                position.x = position.x + Time.deltaTime * speed * direction; ;
+                anim.SetFloat("Move X", direction);
+                anim.SetFloat("Move Y", 0);
+            }
+        }
+
+        rb.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+    public void Fix()
+    {
+        broken = false;
+        rb.simulated = false;
+
+        anim.SetTrigger("Fixed");
+        smokeEffect.Stop();
+
     }
 }
+
