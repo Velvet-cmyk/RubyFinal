e7ed67a27babb2cbca501e3ad0e3c14b94737a83
Velvet-cmyk <you@example.com>
2021-04-01 19:23:28 -0400

    April 1, 2021 7:23 PM

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    AudioSource audioSource
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource=GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
