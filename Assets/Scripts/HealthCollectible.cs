8b5d7e9decf993254f0c2c143e943bb218ecbff2
Velvet-cmyk <you@example.com>
2021-04-02 12:24:19 -0400

    April 2, 2021 12:24 PM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -10,7 +10,7 @@ public class HealthCollectible : MonoBehaviour
 
 
     public GameObject healthEffect;
-    public ParticleSystem health;
+
 
 
 
@@ -37,7 +37,7 @@ public class HealthCollectible : MonoBehaviour
 
                 Destroy(gameObject);
 
-                
+
 
             }
 
