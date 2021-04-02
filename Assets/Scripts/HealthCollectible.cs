a811f8bda20db93560266e64325e27caa485e0d4
Velvet-cmyk <you@example.com>
2021-04-02 04:47:13 -0400

    April 2, 2021 4:47 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -8,8 +8,7 @@ public class HealthCollectible : MonoBehaviour
     public AudioClip collectedClip;
 
     public ParticleSystem healthEffect;
-    public GameObject healthyEffect; 
-
+    public GameObject
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,8 +20,8 @@ public class HealthCollectible : MonoBehaviour
         {
             if (controller.currentHealth < controller.maxHealth)
             {
-                
-              
+
+
 
                 controller.ChangeHealth(1);
 
@@ -35,7 +34,7 @@ public class HealthCollectible : MonoBehaviour
             }
 
         }
-        if (Collision.gameObject.tag="Health")
+        if (Collision.gameObject.tag = "Health")
         {
 
         }
