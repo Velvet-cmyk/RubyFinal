42b925b062de527fc4e2f3ef21411d2477bf6223
Velvet-cmyk <you@example.com>
2021-04-02 13:17:45 -0400

    April 2, 2021 1:17 PM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -31,7 +31,7 @@ public class HealthCollectible : MonoBehaviour
 
 
                 ParticleSystem.Instantiate(healthEffect, transform.position, Quaternion.identity);
-                health.Play();
+
 
                 controller.ChangeHealth(1);
 
