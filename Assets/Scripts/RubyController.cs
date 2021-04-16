571d0588f1bbd71a54441c75711ba80785873177
Velvet-cmyk <you@example.com>
2021-04-16 12:45:02 -0400

    April 16, 2021 12:45 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -40,6 +40,7 @@ public class RubyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         audioSource = GetComponent<AudioSource>();
+        fixedBot=0; 
 
 
 
