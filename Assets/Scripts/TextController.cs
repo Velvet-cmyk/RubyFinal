57502ab3f311932ce38ff7d6c5ff354cc2a3f828
Velvet-cmyk <you@example.com>
2021-04-23 21:59:21 -0400

    April 23, 2021 9:59 PM

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TextController : MonoBehaviour
 {
     public Text scoreValue; 
-    public int 
+    public int score; 
 
     // Start is called before the first frame update
     void Start()
