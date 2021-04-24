a6cd98d674611d8013667af35e9f0fcce08316ab
Velvet-cmyk <you@example.com>
2021-04-23 23:18:45 -0400

    April 23, 2021 11:18 PM

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -23,7 +23,7 @@ public class TextController : MonoBehaviour
     {
         Projectile controller = gameObject.GetComponent<Projectile>();
 
-        if (controller.Collider
+        if (
     }  
         
         
