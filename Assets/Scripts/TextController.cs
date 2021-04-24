531b53a057d1a9f56fad3bc8c91c294ca521bcb0
Velvet-cmyk <you@example.com>
2021-04-23 23:03:41 -0400

    April 23, 2021 11:03 PM

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -1,28 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
-    public Text scoreValue; 
-    private int score; 
+    public Text scoreValue;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        score=0; 
-        scoreValue.text="Fixed:" + score.ToString();  
-    
+        score = 0;
+        scoreValue.text = "Fixed:" + score.ToString();
 
-        
+
+
     }
 
     // Update is called once per frame
     void OnCollisionEnter2D()
     {
-        Projectile controller=gameObject.GetComponent<Projectile>(); 
+        Projectile controller = gameObject.GetComponent<Projectile>();
 
-        if(controller.Collision2D
-    }
+        if (controller.Collider2D
+    }  
+        
+        
+        
 }
+
