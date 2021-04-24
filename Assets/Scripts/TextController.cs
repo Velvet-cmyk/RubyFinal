c3ededc417e70be670bcaa3197a30cf13a4ea694
Velvet-cmyk <you@example.com>
2021-04-23 22:00:20 -0400

    April 23, 2021 10:00 PM

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -11,6 +11,9 @@ public class TextController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        score=0; 
+        scoreValue.text="Fixed:";  
+    
 
         
     }
