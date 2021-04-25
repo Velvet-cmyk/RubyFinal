5fa9214a8e209797b585d61ecadd0bd52873d7b4
Velvet-cmyk <you@example.com>
2021-04-25 14:06:36 -0400

    April 25, 2021 2:06 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -23,7 +23,7 @@ public class RubyController : MonoBehaviour
     AudioSource audioSource;
     public ParticleSystem Effect = null;
     public AudioClip hitClip;
-   
+
 
 
 
@@ -134,21 +134,21 @@ public class RubyController : MonoBehaviour
         GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Vector2.up * 0.5f, Quaternion.identity);
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 300);
-        
+
 
 
         anim.SetTrigger("Launch");
-        if(projectile.)
+
 
 
 
 
     }
 
-    
-    
-    
-    
+
+
+
+
 
 
 
