54fd0091177c7bb8c8ce99a5790421afb076acfc
Velvet-cmyk <you@example.com>
2021-12-05 14:23:04 -0500

    December 5, 2021 2:23 PM

--- Assets/Scripts/TextControllerStage2.cs
+++ Assets/Scripts/TextControllerStage2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TextController : MonoBehaviour
+public class TextControllerStage2 : MonoBehaviour
 {
     public AudioClip musicClipOne;
 
